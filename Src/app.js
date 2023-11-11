b26f882fb59ac1666f021397c9c78fbd7a18ac85
Jordy <105098782+Yoyisn@users.noreply.github.com>
2023-11-10 19:31:39 -0500

    Update app.js

--- Src/app.js
+++ Src/app.js
@@ -13,6 +13,13 @@ app.use(cors({
     credentials: true
 }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://servitechfront.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(morgan('dev'));
 
 app.use(express.json());
